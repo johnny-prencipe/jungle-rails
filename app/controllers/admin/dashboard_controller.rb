be46e4d8f870189241b097174afd73284df407a8
Johnny Prencipe <johnny.prencipe@gmail.com>
2021-02-08 05:34:35 +0000

    Render product info on admin view

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,14 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['HTTP_USER'], password: ENV['HTTP_PASSWORD']
   
   def show
+    @products = Product.all
+    @categories_length = Category.all.length
+
+    @stock = 0
+
+    @products.each do |item|
+      @stock += item.quantity
+    end
+    
   end
 end
